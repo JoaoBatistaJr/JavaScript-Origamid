891519aa1ee3b7e77a1c6784d1f4086677a2af7c
João Batista dos Santos Júnior <joaob.dev@gmail.com>
2022-10-20 14:37:18 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = 'Júnior';
+var idade = '27';
+var comidaFavorita;
+comidaFavorita = 'pizza';
+var peso;
+var altura;
+var dataDeAniversario;
+var statusCivil;
+var statusDeEsperito;
+
+console.log(comidaFavorita);
